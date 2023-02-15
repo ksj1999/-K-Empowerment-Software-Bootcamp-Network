6f98f231e1ad9d92bd340a279c481bc901b88434
kwon <12182193@inha.edu>
2023-02-15 11:26:41 +0900

    Diablo0.2

--- src/Day23.java
+++ src/Day23.java
@@ -1,11 +1,17 @@
 public class Day23 {
     public static void main(String[] args) {
-
+        Barbarian b1 = new Barbarian();
+        Axe berserkerAxe = new Axe();
         Sorceress s1 = new Sorceress();
         Bow windForce = new Bow();
-        s1.setWeapon(windForce);
-        s1.performWeapon();
 
+        s1.setWeapon(windForce);
+        b1.setWeapon(berserkerAxe);
+        s1.performWeapon();
+        b1.performWeapon();
+        s1.setWeapon(new Axe());
+        s1.performWeapon();
+        s1.info();
 
     }
 }

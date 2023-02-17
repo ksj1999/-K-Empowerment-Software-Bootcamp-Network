1360fd96d617a903232f1c9ed4853c5a93f5ef10
kwon <kwon@kayaui-MacBookAir.local>
2023-02-17 18:20:59 +0900

    webserver

--- src/Day24_assignment.java
+++ src/Day24_assignment.java
@@ -1,72 +1,56 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.InetAddress;
 
 public class Day24_assignment {
-    private static List<Socket> clients = new ArrayList<>(); // list of clients connected to the server
 
-    public static void main(String[] args) {
-        System.out.println("Time server is running...");
-        try (ServerSocket serverSocket = new ServerSocket(20000)) {
-            while (true) {
-                System.out.println("Waiting for a new client...");
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Client connected at " + new Date());
-                clients.add(clientSocket);
-                Thread clientThread = new Thread(new ClientHandler(clientSocket));
-                clientThread.start();
+    public Day24_assignment() {
+        System.out.println("HTTP Client Started");
+        try {
+            InetAddress serverInetAddress = InetAddress.getByName("127.0.0.1");
+            Socket connection = new Socket(serverInetAddress, 8080);
+
+            try (OutputStream out = connection.getOutputStream();
+                 BufferedReader in = new BufferedReader(
+                         new InputStreamReader(connection.getInputStream()))) {
+                sendGet(out);
+                System.out.println(getResponse(in));
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 
-    // This inner class handles communication with a single client
-    private static class ClientHandler implements Runnable {
-        private Socket clientSocket;
-
-        public ClientHandler(Socket clientSocket) {
-            this.clientSocket = clientSocket;
+    private void sendGet(OutputStream out) {
+        try {
+            // Send Request
+            out.write("GET /index HTTP/1.0\r\n".getBytes());
+            // Send Header
+            out.write("User-Agent: Mozilla/5.0\r\n".getBytes());
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
+    }
 
-        @Override
-        public void run() {
-            try {
-                while (true) {
-                    // Read input from the client
-                    String inputLine;
-                    try (var in = clientSocket.getInputStream();
-                         var reader = new BufferedReader(new InputStreamReader(in))) {
-                        inputLine = reader.readLine();
-                    }
-
-                    // Check if the client wants to exit
-                    if (inputLine.equalsIgnoreCase("exit")) {
-                        System.out.println("Client disconnected at " + new Date());
-                        break;
-                    }
-
-                    // Send the message to all clients
-                    System.out.println("Message received from client: " + inputLine);
-                    for (Socket client : clients) {
-                        if (client != clientSocket) { // Don't send the message to the same client
-                            try (var out = client.getOutputStream()) {
-                                out.write(inputLine.getBytes());
-                            }
-                        } else {
-                            try (var out = client.getOutputStream()) {
-                                out.write(("You wrote: " + inputLine).getBytes());
-                            }
-                        }
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+    private String getResponse(BufferedReader in) {
+        try {
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine).append("\n");
             }
+            return response.toString();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        new Day24_assignment();
     }
 }
